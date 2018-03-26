449dcd348c497a47ecdc21e93838fa6b9a063cf6
nickhma <nick.ma@3rdave.com>
2018-03-25 20:53:58 -0700

    Add HTML routing

--- pointatnick/website/views.py
+++ pointatnick/website/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-   return HttpResponse("the home page")
+    return render(request, 'website/home.html')
 
 
 def about(request):
