22df6d62f35b7c9ca284916f7cd6cc46f803e12d
nickhma <nick.ma@3rdave.com>
2018-03-25 17:49:07 -0700

    Create /about and /project routes

--- pointatnick/website/views.py
+++ pointatnick/website/views.py
@@ -1,4 +1,12 @@
 from django.http import HttpResponse
 
 def home(request):
-   return HttpResponse("Hello world, you are the home page.")
+   return HttpResponse("the home page")
+
+
+def about(request):
+    return HttpResponse("the about page")
+
+
+def projects(request):
+    return HttpResponse("the projects page")
