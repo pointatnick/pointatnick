15ba0e9b0e8b52d940187f9309663c1f4e7d6892
nickhma <nick.ma@3rdave.com>
2018-03-25 20:57:10 -0700

    Use an HTML file for 'about' page

--- pointatnick/website/views.py
+++ pointatnick/website/views.py
@@ -1,12 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 def home(request):
     return render(request, 'website/home.html')
 
 
 def about(request):
-    return HttpResponse("the about page")
+    return render(request, 'website/about.html')
 
 
 def projects(request):
